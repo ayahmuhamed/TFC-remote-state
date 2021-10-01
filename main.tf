d247c86c3a9d824aa2b4965629bfde6d3c4b6553
ayahmuhamed <88331884+ayahmuhamed@users.noreply.github.com>
2021-10-01 10:30:16 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+data "terraform_remote_state" "random_pet" {
+  backend = "remote"
+
+  config = {
+    organization = "YOUR-ORGANISATION-NAME"
+    workspaces = {
+      name = "YOUR-WORKSPACE-NAME"
+    }
+  }
+}
+
+resource "null_resource" "null" {
+  provisioner "local-exec" {
+    command = "echo The pet name from remote_state is ${data.terraform_remote_state.random_pet.outputs.random_pet}"
+  }
+}
